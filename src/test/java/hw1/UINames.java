f030b5954f9d7d8b5f86917914473279ecf89d90
abbos97 <abbosboev@gmail.com>
2020-06-02 15:25:39 -0400

    Homework 1.

--- src/test/java/hw1/UINames.java
+++ src/test/java/hw1/UINames.java
@@ -1,5 +1,16 @@
 package hw1;
 
+import static io.restassured.RestAssured.*;
+import static org.apache.groovy.util.concurrent.concurrentlinkedhashmap.Weighers.map;
+import static org.hamcrest.Matchers.*;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class UINames {
 
 //    UI names API testing
@@ -9,19 +20,56 @@ public class UINames {
 //    Automate the given test cases. You can use any existing project. You can automate all test cases in
 //    same class or different classes.
 
-
+    @BeforeAll
+    public static void beforeAll(){
+        baseURI = "https://cybertek-ui-names.herokuapp.com/api/";
+    }
 //    TEST CASES
 //    No params test
 //1. Send a get request without providing any parameters
 //2. Verify status code 200, content type application/json; charset=utf-8
 //            3. Verify that name, surname, gender, region fields have value
+@Test
+@DisplayName("No params test")
+public void noParams(){
+    Response response = when().get().prettyPeek();
 
+    response.then().
+            assertThat().
+            statusCode(200).
+            and().
+            contentType("application/json; charset=utf-8").
+    and().
+          body("name", notNullValue()).
+            body("surname",notNullValue()).
+            body("gender",notNullValue()).
+            body("region",notNullValue());
+}
 
 //    Gender test
 //1. Create a request by providing query parameter: gender, male or female
 //2. Verify status code 200, content type application/json; charset=utf-8
 //            3. Verify that value of gender field is same from step 1
-
+private final List<String> genders = Arrays.asList("male","female");
+    @Test
+    @DisplayName("Gender test")
+    public void genderTest() {
+        Collections.shuffle(genders);
+        String gender = genders.get(0);
+        System.out.println("Sending gender as = " + gender);
+        Response response =
+                given().
+                        queryParams("gender",gender).
+                        when().
+                        get().prettyPeek();
+        response.then().
+                assertThat().
+                statusCode(200).
+                and().
+                contentType("application/json; charset=utf-8").
+                and().
+                body("gender",is(gender));
+}
 
 //     2 params test
 //1. Create a request by providing query parameters: a valid region and gender
@@ -29,32 +77,124 @@ public class UINames {
 //2. Verify status code 200, content type application/json; charset=utf-8
 //3. Verify that value of gender field is same from step 1
 //4. Verify that value of region field is same from step 1
+@Test
+    @DisplayName("2 params test")
+    public void twoParamsTest(){
+        Response response=
+                given().queryParam("region","Belgium").
+                        queryParam("gender","female").
+                 when().
+                        get().prettyPeek();
 
+        response.then().
+                assertThat().
+                statusCode(200).
+                and().
+                contentType("application/json; charset=utf-8").
+                and().
+                body("gender",is("female")).
+                body("region",is("Belgium"));
+}
 
 //    Invalid gender test
 //1. Create a request by providing query parameter: invalid gender
 //2. Verify status code 400 and status line contains Bad Request
 //3. Verify that value of error field is Invalid gender
 
+@Test
+    @DisplayName("Invalid gender test")
+    public void invalidGender(){
+    Response response = given().
+            queryParam("gender", "dog").
+            when().get().prettyPeek();
 
+    response.
+            then().
+            assertThat().
+            statusCode(400).
+            and().
+            statusLine(containsString("Bad Request")).
+            and().
+            body("error",is("Invalid gender"));
+}
 //    Invalid region test
 //1. Create a request by providing query parameter: invalid region
 //2. Verify status code 400 and status line contains Bad Request
 //3. Verify that value of error field is Region or language not found
+@Test
+@DisplayName("Invalid region test")
+public void invalidRegion(){
+    Response response = given().
+            queryParam("region", "Miami").
+            when().get().prettyPeek();
 
+    response.
+            then().
+            assertThat().
+            statusCode(400).
+            and().
+            statusLine(containsString("Bad Request")).
+            and().
+            body("error",is("Region or language not found"));
+}
 
 //    Amount and regions test
 //1. Create request by providing query parameters: a valid region and amount (must be bigger than 1)
 //2. Verify status code 200, content type application/json; charset=utf-8
 //3. Verify that all objects have different name+surname combination
 
-
+    @Test
+    @DisplayName("Amount and regions test")
+    public void amountAndRegion() {
+        Response response =
+                given().
+                        queryParams("region","Argentina").
+                        queryParams("amount",300).
+                        when().
+                        get().prettyPeek();
+        List<User>userList = response.jsonPath().getList("",User.class);
+        System.out.println("userList = " + userList);
+        Set<String> fullNames = new HashSet<>();
+        for (User user:userList) {
+            String fullName = user.getName() + " " + user.getSurname();
+            fullNames.add(fullName);
+        }
+        //2nd way
+        Set<String> fullNamesV2 = userList.stream().
+                map(user -> user.getName() + " " + user.getSurname() ).
+                collect(Collectors.toSet());
+        response.then().
+                assertThat().
+                statusCode(200).
+                and().
+                header("Content-Type","application/json; charset=utf-8").
+                and().
+                body("size()",is(fullNames.size()));
+}
 //      3 params test
 //1. Create a request by providing query parameters: a valid region, gender and amount (must be bigger
 //            than 1)
 //2. Verify status code 200, content type application/json; charset=utf-8
 //3. Verify that all objects the response have the same region and gender passed in step 1
+@Test
+@DisplayName("3 params test")
+    public void threeParamsTest(){
+    Response response = given().
+            queryParams("region", "France").
+            queryParams("gender", "male").
+            queryParam("amount", 20).
+            when().get().prettyPeek();
 
+    response.then().
+            assertThat().
+            statusCode(200).
+            and().
+            contentType("application/json; charset=utf-8").
+            and().
+            body("size()",is(20)).
+            body("gender",everyItem(is("male"))).
+            body("region",everyItem(is("France")));
+}
 
 //    Amount count test
 //1. Create a request by providing query parameter: amount (must be bigger than 1)
---
+++ src/test/java/hw1/User.java
@@ -0,0 +1,30 @@
+package hw1;
+
+import java.util.Objects;
+
+/*
+"name": "Dominic",
+        "surname": "Lorenz",
+        "gender": "male",
+        "region": "Germany"
+ */
+public class User {
+    private String name;
+    private String surname;
+    private String gender;
+    private String region;
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public String getRegion() {
+        return region;
+
+    }
+}
